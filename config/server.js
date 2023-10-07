bc5ff4acccf781c6a5e797a3bf1b2463a9acd105
Marcos Magalhães <marcosvmagalhaes123@gmail.com>
2023-10-07 14:59:34 -0300

    incluindo css

--- config/server.js
+++ config/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + '\\..\\public'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
